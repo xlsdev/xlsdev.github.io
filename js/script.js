50f10d6091eff9e8a7851f1e2e412bf74cb2b030
ihmwhydn <ihm245@gmail.com>
2022-10-17 09:30:57 +0700

    test

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
   $(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if (scroll > 64){
-      $(".navbar").addClass('bg-black-2');
+      $(".navbar").removeClass('bg-trasnparent').addClass('bg-black-2');
     } else{
-      $(".navbar").removeClass('bg-black-2');
+      $(".navbar").removeClass('bg-black-2').addClass('bg-trasnparent');
     }
   })
 
   $('.navbar-toggler').click(function(){
-    $('.navbar').toggleClass('bg-black');
+    $('.navbar').removeClass('bg-trasnparent').toggleClass('bg-black');
   });
 
 });
